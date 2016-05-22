83561d55e6567429a06e32cf7fb0a16c7e657bf0
Steven Kessler <stvnksslr@gmail.com>
2016-05-22 13:49:41 -0400

    feat(adding simple health tracking for objects): currently only the object being collided into will have its health tracked this seems kinda backwards to what im looking to do so this will definatly be revisted

--- Assets/src/PlayerMovement/Damage/DamageHandler.cs
+++ Assets/src/PlayerMovement/Damage/DamageHandler.cs
@@ -3,7 +3,34 @@ using System.Collections;
 
 public class DamageHandler : MonoBehaviour {
 
+	int health = 10;
+
+
 	void OnCollisionEnter2D(Collision2D coll) {
 		Debug.Log ("Collision!");
+
+		if (coll.gameObject.tag == "Player") {
+			Debug.Log ("Player has taken damage!");
+			--health;
+			Debug.Log ("Object Current Health: " + health);
+
+			if (health <= 0) {
+				Die();
+			}
+		}
+
+	 	if (coll.gameObject.tag == "Enemy") {
+			Debug.Log ("Enemy Has taken damage!");
+		}
+
+		if (coll.gameObject.tag == "Asteroid") {
+			Debug.Log ("Asteroids dont take damage!");
+		}
+
 	}
+
+	void Die() {
+		Destroy (gameObject);
+	}
+
 }
